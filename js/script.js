cd1e4d36951d970fbe49da9085f3fc24e0ab38dd
Erna <etusliyanti@gmail.com>
2024-01-10 15:24:03 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Toggle clss active
+const navbarNav = document.querySelector ('.navbar-nav');
+
+// Ketika humberger menu di klik
+document.querySelector('#hamberger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+
+};
+
+// klik diluar sidebar untuk menghilangkan nav
+const hamberger = document.querySelector('#hamberger-menu');
+
+document.addEventListener('click', function (e){
+    if (!hamberger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+        
+    }
+});
